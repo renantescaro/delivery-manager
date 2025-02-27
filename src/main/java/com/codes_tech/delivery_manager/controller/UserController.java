a3795319571e5f9d403d6c187a4e8c91534e7717
Renan Tescaro <renantescaro@yahoo.com.br>
2025-02-27 15:51:25 -0300

    feat: edit page

--- src/main/java/com/codes_tech/delivery_manager/controller/UserController.java
+++ src/main/java/com/codes_tech/delivery_manager/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.codes_tech.delivery_manager.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,7 +15,7 @@ import com.codes_tech.delivery_manager.model.User;
 import com.codes_tech.delivery_manager.repository.UserRepository;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/panel/user")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -22,10 +25,9 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list(Model model) {
+    public String listRender(Model model) {
         List<User> users = userRepository.findAll();
         model.addAttribute("users", users);
-
         return "user/index";
     }
 
@@ -35,9 +37,32 @@ public class UserController {
         return "user/new";
     }
 
+    @RequestMapping(path = "/edit/{id}", method = RequestMethod.GET)
+    public String editRender(@PathVariable Long id, Model model) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            model.addAttribute("user", user.get());
+            return "user/edit";
+        }
+        return "redirect:/panel/user";
+    }
+
     @RequestMapping(path = "/new", method = RequestMethod.POST)
     public String insert(@ModelAttribute User user) {
         userRepository.save(user);
-        return "redirect:/user";
+        return "redirect:/panel/user";
+    }
+
+    @RequestMapping(path = "/edit/{id}", method = RequestMethod.POST)
+    public String update(@PathVariable Long id, @ModelAttribute User user) {
+        Optional<User> existingUser = userRepository.findById(id);
+        if (existingUser.isPresent()) {
+            User updatedUser = existingUser.get();
+            updatedUser.setUsername(user.getUsername());
+            updatedUser.setEmail(user.getEmail());
+            updatedUser.setPassword(user.getPassword());
+            userRepository.save(updatedUser);
+        }
+        return "redirect:/panel/user";
     }
 }
